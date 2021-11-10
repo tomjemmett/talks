7c06006139d1fbb97aae192a2f28a999a84d9d1b
Tom Jemmett <tom.jemmett@gmail.com>
2021-11-10 11:42:57 +0000

    removes need for here package, changes storage of icon set

--- .Rprofile
+++ .Rprofile
@@ -2,6 +2,5 @@ source("renv/activate.R")
 
 # {icons}
 # set the icon.path option to save to a specific folder which we can cache
-if (renv::status()$synchronized) {
-  options("icon.path" = here::here("icons"))
-}
+# would like to use here, but this script may get loaded before renv::restore()
+options("icon.path" = file.path(getwd(), "icons"))
--- get_icons.R
+++ get_icons.R
@@ -1,8 +1,13 @@
 library(icons)
 
 icon_path <- getOption("icon.path")
+my_icons_path <- file.path(icon_path, "my_icons")
+
 if (!dir.exists(icon_path)) {
   dir.create(icon_path)
+}
+if (!dir.exists(my_icons_path)) {
+  dir.create(my_icons_path)
 }
 
 # make sure the icons are downloaded
@@ -17,4 +22,4 @@ icon_save(list(
   gh = fontawesome$brands$github,
   tw = fontawesome$brands$twitter,
   li = fontawesome$brands$linkedin
-), icon_path)
+), my_icons_path)
