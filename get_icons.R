6fe5073998dd788d2aac04fc0a4d04d4741c20af
Tom Jemmett <tom.jemmett@gmail.com>
2021-11-10 10:00:21 +0000

    hopefully makes icon downloading work

--- get_icons.R
+++ get_icons.R
@@ -1,20 +1,22 @@
 library(icons)
 
-# skip if the folder exists
+# set the icon.path option to save to a specific folder which we can cache
+options("icon.path" = "icons")
+
 if (!dir.exists("icons")) {
   dir.create("icons")
-
-  # make sure the icons are downloaded
-  if (!icon_installed(fontawesome)) {
-    download_fontawesome()
-  }
-
-  # list any icons we want to use here
-  icon_save(list(
-    em = fontawesome$regular$envelope,
-    de = fontawesome$solid$desktop,
-    gh = fontawesome$brands$github,
-    tw = fontawesome$brands$twitter,
-    li = fontawesome$brands$linkedin
-  ), "icons")
 }
+
+# make sure the icons are downloaded
+if (!icon_installed(fontawesome)) {
+  download_fontawesome()
+}
+
+# list any icons we want to use here
+icon_save(list(
+  em = fontawesome$regular$envelope,
+  de = fontawesome$solid$desktop,
+  gh = fontawesome$brands$github,
+  tw = fontawesome$brands$twitter,
+  li = fontawesome$brands$linkedin
+), "icons")
