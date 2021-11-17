580644ef7c40775995557f0caa38e1de21ee2146
Tom Jemmett <tom.jemmett@gmail.com>
2021-11-17 11:12:45 +0000

    switches to using my new actions

--- .Rprofile
+++ .Rprofile
@@ -1,6 +1,1 @@
 source("renv/activate.R")
-
-# {icons}
-# set the icon.path option to save to a specific folder which we can cache
-# would like to use here, but this script may get loaded before renv::restore()
-options("icon.path" = file.path(getwd(), "icons"))
---
+++ system_requirements.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# system requirements for packages
+
+apt-get install -y libxml2-dev
+apt-get install -y make
+apt-get install -y libcurl4-openssl-dev
+apt-get install -y libssl-dev
+apt-get install -y zlib1g-dev
+apt-get install -y pandoc
+apt-get install -y libicu-dev
